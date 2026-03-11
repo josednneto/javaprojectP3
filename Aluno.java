a7d322ba93185e770ea20524abbd5d60ecda6e58
José Neto <josednn15@hotmail.com>
2026-03-11 11:01:24 -0300

    Update Aluno.java
    Atualizção
    - adc prazoDevolucao

--- Aluno.java
+++ Aluno.java
@@ -1,8 +1,14 @@
 public class Aluno extends Usuario {
+
     private String matricula;
 
-    public Aluno(String nome, String cpf, String email, String matricula) {
-        super(nome, cpf, email);
+    public Aluno(String nome, String cpf, String matricula) {
+        super(nome, cpf);
         this.matricula = matricula;
+    }
+
+    @Override
+    public int prazoDevolucao() {
+        return 15;
     }
 }
