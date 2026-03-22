f0fb3844acf9facd977228cb6e6a2b6da8edd453
José Neto <josednn15@hotmail.com>
2026-03-22 12:52:55 -0300

    Rename Main class to Program and modify book logic
    Renamed class Main to Program and updated book handling.
    
    Atualização:
    - Renomeado a Classe para Program;
    - Testando novo método: Relatório.

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Program {
 
     public static void main(String[] args) {
 
@@ -8,16 +8,20 @@ public class Main {
         Professor professor = new Professor("Ana", "222", "Computação");
 
         Livro livro1 = new Livro("Java POO", "Deitel");
-
+        Livro livro2 = new Livro("Entendendo Algoritmos", "Aditya Y.");
+        
         biblioteca.adicionarUsuario(aluno);
         biblioteca.adicionarUsuario(professor);
 
         biblioteca.adicionarLivro(livro1);
-
+        biblioteca.adicionarLivro(livro2);
+        
         biblioteca.emprestarLivro(aluno, livro1);
-
+        biblioteca.emprestarLivro(professor, livro2);
         System.out.println();
 
         biblioteca.devolverLivro(livro1);
+        
+        System.out.println();
+        biblioteca.relatorio();
     }
-}
